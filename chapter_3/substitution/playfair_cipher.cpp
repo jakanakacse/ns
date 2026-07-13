cf8628675faff2a3f001f5f1912622ed54b61ab6
Jakanaka CSE <shazid.test@gmail.com>
2026-07-14 02:51:49 +0600

    Update playfair_cipher.cpp

--- chapter_3/substitution/playfair_cipher.cpp
+++ chapter_3/substitution/playfair_cipher.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<char>> playfair_matrix = {
+vector<vector<char>> mat = {
     {'L', 'I', 'S', 'T', 'E'},
     {'R', 'A', 'B', 'C', 'D'},
     {'F', 'G', 'H', 'K', 'M'},
@@ -12,7 +12,7 @@ vector<vector<char>> playfair_matrix = {
 string prepare_text(string text) 
 {
     string result = "";
-    for (size_t i = 0; i < text.size(); ) {
+    for (int i = 0; i < text.size(); ++i) {
         if (isalpha(text[i])) {
             char ch = toupper(text[i]);
             if (ch == 'J') ch = 'I';
@@ -20,9 +20,6 @@ string prepare_text(string text)
             if (i+1 < text.size() && toupper(text[i+1]) == toupper(text[i])) {
                 result += 'X';
             }
-            ++i;
-        } else {
-            ++i;
         }
     }
     if (result.size() % 2 != 0) result += 'X';
@@ -33,30 +30,27 @@ pair<int, int> find_position(char c)
 {
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
-            if (playfair_matrix[i][j] == c) return {i, j};
+            if (mat[i][j] == c) return {i, j};
         }
     }
-    return {-1, -1};
 }
 
 string encrypt(string plaintext) 
 {
     string text = prepare_text(plaintext);
     string result = "";
-    for (size_t i = 0; i < text.size(); i += 2) {
-        char a = text[i], b = text[i+1];
-        int row1, col1, row2, col2;
-        tie(row1, col1) = find_position(a);
-        tie(row2, col2) = find_position(b);
-        if (row1 == row2) {
-            result += playfair_matrix[row1][(col1+1)%5];
-            result += playfair_matrix[row2][(col2+1)%5];
-        } else if (col1 == col2) {
-            result += playfair_matrix[(row1+1)%5][col1];
-            result += playfair_matrix[(row2+1)%5][col2];
+    for (int i = 0; i < text.size(); i += 2) {
+        auto a = find_position(text[i]);
+        auto b = find_position(text[i + 1]);
+        if (a.first == b.first) {
+            result += mat[a.first][(a.second+1)%5];
+            result += mat[b.first][(b.second+1)%5];
+        } else if (a.second == b.second) {
+            result += mat[(a.first+1)%5][a.second];
+            result += mat[(b.first+1)%5][b.second];
         } else {
-            result += playfair_matrix[row1][col2];
-            result += playfair_matrix[row2][col1];
+            result += mat[a.first][b.second];
+            result += mat[b.first][a.second];
         }
     }
     return result;
@@ -65,20 +59,18 @@ string encrypt(string plaintext)
 string decrypt(string ciphertext) 
 {
     string result = "";
-    for (size_t i = 0; i < ciphertext.size(); i += 2) {
-        char a = toupper(ciphertext[i]), b = toupper(ciphertext[i+1]);
-        int row1, col1, row2, col2;
-        tie(row1, col1) = find_position(a);
-        tie(row2, col2) = find_position(b);
-        if (row1 == row2) {
-            result += playfair_matrix[row1][(col1+4)%5];
-            result += playfair_matrix[row2][(col2+4)%5];
-        } else if (col1 == col2) {
-            result += playfair_matrix[(row1+4)%5][col1];
-            result += playfair_matrix[(row2+4)%5][col2];
+    for (int i = 0; i < ciphertext.size(); i += 2) {
+        auto a = find_position(toupper(ciphertext[i]));
+        auto b = find_position(toupper(ciphertext[i + 1]));
+        if (a.first == b.first) {
+            result += mat[a.first][(a.second + 4) % 5];
+            result += mat[b.first][(b.second + 4) % 5];
+        } else if (a.second == b.second) {
+            result += mat[(a.first + 4) % 5][a.second];
+            result += mat[(b.first + 4) % 5][b.second];
         } else {
-            result += playfair_matrix[row1][col2];
-            result += playfair_matrix[row2][col1];
+            result += mat[a.first][b.second];
+            result += mat[b.first][a.second];
         }
     }
     return result;
@@ -87,7 +79,7 @@ string decrypt(string ciphertext)
 void print_matrix() 
 {
     cout << "Playfair Matrix:" << endl;
-    for (auto& row : playfair_matrix) {
+    for (auto& row : mat) {
         for (char c : row) cout << c << ' ';
         cout << endl;
     }
